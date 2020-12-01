0b8f8274bc3cd37abdd43ac2d84bdfda357387ba
RadSton <55923402+RadSton@users.noreply.github.com>
2020-12-01 07:58:52 +0100

    Update main.js

--- boomgame/main.js
+++ boomgame/main.js
@@ -4,75 +4,73 @@ const ctx = canvas.getContext("2d");
 const prb = getObject("button")
 // VARS
 var bombs = [];
-var ba = 0;
+var nuclearbombs = [];
+var bombtype = getObject(".bombtype").checked;
 // CODE
 
-function update() {
-  if(ba == 1) {
-    if(bombs.length != 0) {
-      bombs.forEach(function(item, index) {
-        item.clear(ctx);
-      })
-    }
-  }
-}
 
 function render() {
   renderBomb();
+  renderNuclearBomb();
 }
 
 function mousedown(event) {
-  createBomb(getPos(canvas,event))
+  bombtype = getObject(".bombtype").checked;
+  if(bombtype == false)
+    createBomb(getPos(canvas,event))
+  else
+    createNuclearBomb(getPos(canvas,event))
 }
 
-// BOMB
+// NORMAL BOMB
 function renderBomb() {
   if(bombs.length != 0) {
-    bombs.forEach(function(item, index) {
-      item.render(ctx);
-    })
+    drawBomb(bombs,ctx);
   }
 }
 
-function prime() {
-  ba = 1;
-  if(bombs.length != 0) {
-    bombs.forEach(function(item, index) {
-      item.prime();
-    })
-  }
+function primeBombs() {
+  bombs[0].pt = 1;
+  clearBomb(bombs,ctx);
   setTimeout(unprime,1000)
 }
 
 function unprime() {
-  console.log("hi")
-  ba = 0;
-  if(bombs.length != 0) {
-    nea = [];
-    bombs.forEach(function(item, index) {
-      if(item.pt == 0) {
-        nea.push(item);
-      } else {
-        item.clear(ctx);
-      }
-    })
-    bombs = nea;
-  }
+  clearBomb(bombs,ctx);
+  bombs = [];
 }
 
 function createBomb(pos) {
   var bomb = new Bomb(pos.x,pos.y);
   bombs.push(bomb)
 }
+
+// NUCLEAR Bomb
+
+function renderNuclearBomb() {
+  if(nuclearbombs.length != 0)
+    drawNuclearBomb(nuclearbombs,ctx);
+}
+
+function primeNuclear() {
+  nuclearbombs[0].pt = 1;
+  setTimeout(unprimenuclear,2000);
+}
+function unprimenuclear() {
+  nuclearbombs = [];
+}
+
+function createNuclearBomb(pos) {
+  var nuclear = new Nuclearbomb(pos.x,pos.y);
+  nuclearbombs.push(nuclear);
+}
 // INIT CODE
 function startGameEngine() {
-  update();
   render();
-  setTimeout(startGameEngine, 100);
+  setTimeout(startGameEngine, 40);
 }
 // EVENTS
 canvas.addEventListener('mousedown',function(e){mousedown(event)});
-prb.addEventListener('click', function(e){prime()});
+prb.addEventListener('click', function(e){if(bombtype == false) primeBombs(); else primeNuclear()});
 // START
 startGameEngine();
- 
