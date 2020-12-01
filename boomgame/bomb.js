505dd58630d4d375e02408fd7b7f788b98f6a6ba
RadSton <55923402+RadSton@users.noreply.github.com>
2020-12-01 07:57:19 +0100

    Update bomb.js

--- boomgame/bomb.js
+++ boomgame/bomb.js
@@ -4,20 +4,5 @@ class Bomb {
     this.y = y;
     this.pt = 0;
   }
-  render(ctx) {
-    if(this.pt == 0) {
-      drawBomb(this.x,this.y,ctx)
-    }
-    if(this.pt == 1) {
-        drawBombExplusion(this.x,this.y,ctx)
-    }
-  }
-
-
-  clear(ctx) {
-    ctx.clearRect(this.x -(128/2), this.y - (120/2), 150, 150);
-  }
-  prime() {
-    this.pt = 1;
-  }
 }
+ 
