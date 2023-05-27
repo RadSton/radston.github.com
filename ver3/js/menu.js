ab1eb2ada0639848fd2beb3cf10ac3add4db0ff9
radston12 <radston12@gmail.com>
2023-05-27 12:05:25 +0200

    Added usage data

--- ver3/js/menu.js
+++ ver3/js/menu.js
@@ -24,6 +24,7 @@ const menus = [
 ]
 
 let INDEX_OF_TESTING = 1; // menus[1] -> testing
+let initalLoad = Date.now();
 
 const hideAll = () => {
     for (const menu of menus) {
@@ -66,3 +67,33 @@ for (const menu of menus) {
 const isTestingSelectedInMenu = () => {
     return !document.querySelector(".testing").classList.contains("hidden")
 }
+
+document.addEventListener("beforeunload", () => {
+   
+})
+
+var MAX_WAIT_MS = 1000;
+var _waitForFinalHit = false;
+
+function recordFinalHit() {
+    _waitForFinalHit = true;
+
+    gtag('event', 'closedpage', {
+        duration: Math.floor(Date.now() - initalLoad / 1000),
+        hitCallback: function() {
+            _waitForFinalHit = false;
+        }
+    });
+}
+
+function waitForFinalHit() {
+    var waitStart = new Date().getTime();
+    while (_waitForFinalHit
+        && (new Date().getTime() - waitStart < MAX_WAIT_MS)) { }
+}
+
+
+window.onunload = () => {
+    recordFinalHit();
+    waitForFinalHit();
+};
